f64b60fcad9a07676a7163518a8deb60bbbd2b2c
EliabParra <117409776+EliabParra@users.noreply.github.com>
2024-04-24 13:10:17 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,71 @@
+from ursina import *
+from ursina.prefabs.first_person_controller import FirstPersonController
+from ursina.shaders import lit_with_shadows_shader
+
+app = Ursina(borderless=False)
+random.seed(0)
+window.size=(1600,800)
+Entity.default_shader = lit_with_shadows_shader
+
+player = FirstPersonController(collider = 'box')
+
+initialPosition = Vec3(0,5,0)
+
+class Cubo(Entity):
+    def __init__(self, position=(0,0,0)):
+        super().__init__(
+            position = position,
+            model = 'cube',
+            scale = (1,1),
+            origin_y = -.5,
+            color = color.violet,
+            collider = 'box',
+            texture = 'white_cube',
+        )
+        
+Cubo(position=(0,1,0))
+Cubo(position=(64,1,64))
+
+player.position = Vec3(0,2,0)
+
+def input(key):
+    if key == 'escape':
+        quit()
+        
+for z in range(47):
+    cubo = Cubo(position=(random.randint(-2,2),random.randint(-1,3),z+2))
+    if z == 46:
+        cubo.color = color.green
+        meta1 = cubo
+        
+for y in range(40):
+    cubo = Cubo(position=(random.randint(63,66),(y+1)*1.2,random.randint(62,65)))
+    if y == 39:
+        cubo.color = color.green
+        meta2 = cubo
+
+for x in range(40):
+    cubo = Cubo(position=(random.randint(63,66),(y+1)*1.2,random.randint(62,65)))
+    if y == 39:
+        cubo.color = color.green
+        meta2 = cubo
+
+ground = Entity(
+    model = 'plane',
+    collider = 'box',
+    color = color.red,
+    scale = 256
+)
+ground.position = Vec3(0,-20,0)
+
+def update():
+    if(player.position.y <= -10):
+        player.position = Vec3(64,3,64)
+    if player.intersects(meta1):
+        player.position = Vec3(64,3,64)
+    if player.intersects(meta2):
+        player.position = Vec3(64,1,64)
+
+Sky()
+
+app.run()
